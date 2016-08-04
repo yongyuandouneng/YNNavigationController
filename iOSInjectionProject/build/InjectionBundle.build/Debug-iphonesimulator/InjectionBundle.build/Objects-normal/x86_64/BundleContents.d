cd2d826f1f05b313477c84d84b34d7214b8046bd
ZYN <zyn@MacBook-Pro.local>
2016-08-04 15:30:22 +0800

    删除多余文件。

--- iOSInjectionProject/build/InjectionBundle.build/Debug-iphonesimulator/InjectionBundle.build/Objects-normal/x86_64/BundleContents.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/zyn/Documents/Xcode7.2_Test_Demo/YNNavigationController/iOSInjectionProject/BundleContents.m \
-  /tmp/injectionforxcode/BundleInjection.h \
-  /tmp/injectionforxcode/BundleInterface.h \
-  /Users/zyn/Documents/Xcode7.2_Test_Demo/YNNavigationController/iOSInjectionProject/BundleContents.h
